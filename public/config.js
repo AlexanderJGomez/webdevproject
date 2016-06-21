7c277fb250472c579f173162a2e9e4bdec3d1d28
Michael Rascati <rascatimichael@gmail.com>
2016-06-21 16:57:14 -0400

    config

--- public/config.js
+++ public/config.js
@@ -32,8 +32,6 @@
             })
             /*
             .when("/user", { // from the fb stuff. could use this for other things to visit other peoples' profile. if you have the id, visit other person's profile- dont need to be logged in. if you dont have id goto own profile
-=======
->>>>>>> 2d0ed7f897dfda53d4259cce1583ec865f6422b9
                 templateUrl: "views/user/profile.view.client.html",
                 controller: "ProfileController",
                 controllerAs: "model",
@@ -64,6 +62,6 @@
                     })
 
             deferred.promise;
-        }
+        }*/
     }
 })();
